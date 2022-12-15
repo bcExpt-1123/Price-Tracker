fd6781e7fed36cdd25669cef1d4d9c4522a78994
saliherdemk <saliherdem_kaymak@hotmail.com>
2022-12-15 20:22:56 +0300

    Initial commit

--- shoes/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Shoe, LinkUrlToUser
-# Register your models here.
-
-admin.site.register(Shoe)
-admin.site.register(LinkUrlToUser)
--- shoes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-app_name = "shoes"
-
-urlpatterns = [path("", views.index, name="index"),
-               path("watch", views.watch, name="watch"),
-               path("unwatch", views.unwatch, name="unwatch")]
